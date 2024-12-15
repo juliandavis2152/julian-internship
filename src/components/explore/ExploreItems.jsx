dba022252eaf267030b5bef332246ce1eb5b828f
juliandavis2152 <juliandavis2152@gmail.com>
2024-12-15 16:22:14 -0700

    Issue with React Declaration Resolved

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
